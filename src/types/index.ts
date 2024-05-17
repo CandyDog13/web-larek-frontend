3fb4d444d47c0ba376ed7b541dc57fab2c53766a
CandyDog13 <kga13@yandex.ru>
2024-05-18 00:19:54 +0300

    Описание и интерфейсов программы. Описание классов данных. Начало описания классов представления

---
+++ src/types/index.ts
@@ -0,0 +1,51 @@
+export interface IProduct {
+    title: string;
+    description: string;
+    price: number;
+    image: string;
+    category: string;
+    _id: string;
+}
+
+export interface IUser {
+    payment: string;
+    address: string;
+    email: string;
+    number: string;
+}
+
+export interface IOrder {
+    _id: string;
+    total: number;
+}
+
+export interface IProductList {
+    products: IProduct[];
+    total: number;
+    preview: string | null;
+    getProduct(productId:string): IProduct;
+
+}
+
+export interface IBasketModel {
+    totalOrder:TOrderTotal;
+    items: Map<string, number>;
+    addProduct(id:string):void;
+    removeProduct(id:string):void;
+}
+
+export interface IUserData {
+    setUserPayAddress(userData:TUserPayAddress):void;
+    setUserEmailPhone(userData:TUserEmailTelephone):void;
+    checkUserValidation(data:Record<keyof IUser, string>):boolean;
+}
+
+export type TProductMainPage = Pick<IProduct, 'title' | 'price' | 'image' | 'category'>
+
+export type TOrderTotal = Pick<IOrder, 'total'>
+
+export type TBasket = Pick<IProduct, 'title'| 'price'> & TOrderTotal
+
+export type TUserPayAddress = Pick<IUser, 'address' | 'payment'>
+
+export type TUserEmailTelephone = Pick<IUser, 'email' | 'number'>
